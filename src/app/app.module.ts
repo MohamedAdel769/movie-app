0af7227cefd0938ebee7f1570855ceb16ddce792
Mohamed Adel <Mohamed.adel90211@gmail.com>
2021-09-20 15:19:18 +0200

    Feature -Added form validation-

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material/material.module";
+import {FormsModule} from "@angular/forms";
+
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { MovieListComponent } from './catalog/movie-list/movie-list.component';
@@ -12,6 +13,7 @@ import { MovieDetailsComponent } from './catalog/movie-details/movie-details.com
 import { MovieItemComponent } from './catalog/movie-list/movie-item/movie-item.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { CarouselComponent } from './carousel/carousel.component';
+
 
 @NgModule({
   declarations: [
@@ -27,6 +29,7 @@ import { CarouselComponent } from './carousel/carousel.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     MaterialModule,
   ],
