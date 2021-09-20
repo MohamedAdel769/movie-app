de43d98ec8667d44478fb97bb605438f1a0ab5b5
Mohamed Adel <Mohamed.adel90211@gmail.com>
2021-09-20 07:36:41 +0200

    Feature -Finished app layout-

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CarouselComponent} from "./carousel/carousel.component";
+import {CatalogComponent} from "./catalog/catalog.component";
+import {UserLoginComponent} from "./user-login/user-login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CarouselComponent},
+  {path: 'catalog', component: CatalogComponent},
+  {path: 'login', component: UserLoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
