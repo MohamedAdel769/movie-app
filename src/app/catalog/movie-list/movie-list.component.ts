5a2276eb91990e66259cab12f31b4e4ab05c24ea
Mohamed Adel <Mohamed.adel90211@gmail.com>
2021-09-21 14:28:44 +0200

    Fix -Added movie list dynamically-

--- src/app/catalog/movie-list/movie-list.component.ts
+++ src/app/catalog/movie-list/movie-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Movie} from "../../shared/movie.model";
+import {MovieDbService} from "../../shared/movie-db.service";
 
 @Component({
   selector: 'app-movie-list',
@@ -6,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
+  movieList: Movie[] = [];
 
-  constructor() { }
+  constructor(private movieDB: MovieDbService) {
+    this.movieList = this.movieDB.movies;
+  }
 
   ngOnInit(): void {
   }
